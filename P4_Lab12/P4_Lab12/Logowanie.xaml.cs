b41786259886d8ba5bbe158ec1db3886d69f3e99
Zawi47 <zawislak.jakub2@gmail.com>
2020-05-20 15:08:22 +0200

    Upload

---
+++ P4_Lab12/P4_Lab12/Logowanie.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P4_Lab12
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Logowanie.xaml
+    /// </summary>
+    public partial class Logowanie : Window
+    {
+        public bool log=false;
+        public User[] users = { new User() { Login = "master", Password = "chybaty" }, new User() { Login = "cienias", Password = "cienias" }, new User() { Login = "ala", Password = "makota" } };
+        public MainWindow MyParent { get; set; }
+        public Logowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var val = new Validation();
+            for (int i= 0;i<3;i++)
+            {
+                if (log == false)
+                {
+                    val.Login = users[i].Login;
+                    val.Password = users[i].Password;
+                    log = val.CheckLogin(textbox1.Text,Passwordbox1.SecurePassword);
+                }
+                
+            }
+            if(log==true)
+            {
+                this.Close();
+                MyParent.superbutton.Content = "wyloguj";
+            }
+            else
+            {
+                MessageBox.Show("Złe logowanie");
+            }
+        }
+    }
+}
---
+++ P4_Lab12/P4_Lab12/Validation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Lab12
+{
+    public class Validation
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+       
+
+        public bool CheckLogin(string login, SecureString password)
+        {
+            var pwd = default(string);
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(password);
+                pwd = Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+            return (this.Login == login && this.Password == pwd);
+        }
+    }
+}
